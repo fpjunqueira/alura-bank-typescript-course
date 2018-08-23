8bd27addde3d867f2a59c84b0b237e077bc656b5
Felipe Junqueira <felipetiuzanata@gmail.com>
2018-08-22 23:47:29 -0300

    Mensagem simples de sucesso

---
+++ app/ts/views/MensagemView.ts
@@ -0,0 +1,20 @@
+class MensagemView {
+
+    private _elemento: Element;
+
+    constructor(seletor: string) {
+        
+        this._elemento = document.querySelector(seletor);
+    }
+
+    update(modelo: string): void {
+
+        this._elemento.innerHTML = this.template(modelo);
+    }
+
+    template(model: string) {
+
+        return `<p class="alert alert-info">${model}</p>`;
+    }
+
+}
