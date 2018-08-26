2772c224fd488a73b7fe1053fa9444f1e4838c39
Felipe Junqueira <felipetiuzanata@gmail.com>
2018-08-26 19:39:04 -0300

    acertando escopo do campo _elemento para private e tornando a classe view uma classe abstrata

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,19 +1,16 @@
-class View<T> {
+abstract class View<T> {
 
-    protected _elemento: Element;
+    private _elemento: Element;
 
     constructor(seletor: string) {
 
         this._elemento = document.querySelector(seletor);
     }
 
-    update(model: T) {
+    update(model: T): void {
 
         this._elemento.innerHTML = this.template(model);
     }
 
-    template(model: T): string {
-
-        throw new Error('Você deve implementar o método template');
-    }
+    abstract template(model: T): string;
 }
